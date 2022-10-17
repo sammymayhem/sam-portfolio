96ce2c7818114d8d5c74462a7ddebd77c5dc0695
Sam Wallace <sam@sammymayhem.com>
2022-10-17 11:56:51 -0700

    Added logo to navbar, changed home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,13 @@ function Header() {
     return (
         <header class='header'>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand class='link1' id='nav-link' as={Link} to='/'>Sam Wallace</Navbar.Brand>
+                <Navbar.Brand class='link1' id='nav-link' as={Link} to='/'>SW</Navbar.Brand>
                 <Container class='nav'>
                     <Nav className="justify-content-end">
-                        <Link class='link3' id='nav-link' as={Link} to='/about'>About</Link>
-                        <Link class='link4' id='nav-link' as={Link} to='/projects'>Projects</Link>
-                        <Link class='link5' id='nav-link' as={Link} to='/contact'>Contact Me</Link>
-                        <Link class='link6' id='nav-link' as={Link} to='/resume'>Resume</Link>
+                        <Link class='link2' id='nav-link' as={Link} to='/about'>About</Link>
+                        <Link class='link3' id='nav-link' as={Link} to='/projects'>Projects</Link>
+                        <Link class='link4' id='nav-link' as={Link} to='/contact'>Contact Me</Link>
+                        <Link class='link5' id='nav-link' as={Link} to='/resume'>Resume</Link>
                     </Nav>
                 </Container>
             </Navbar>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,8 +4,8 @@ function Home() {
     return (
         <div>
             <div id='home-text'>
-                <h2 id='home-text-1'>Hi, my name is</h2>
-                <h1 id='home-text-2'>Sam Wallace</h1>
+                <h1 id='home-text-1'>Sam Wallace.</h1>
+                <h2 id='home-text-2'>Front End Web Developer kinda guy.</h2>
             </div>
         </div>
     );
