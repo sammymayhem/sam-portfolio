22892a07352846765dd0654471f785beecd15c49
Sam Wallace <sam@sammymayhem.com>
2022-10-19 15:24:54 -0700

    border on links on hover fixed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from './components/Footer';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Projects from './components/pages/Projects';
-import Contact from './components/pages/Contact';
+import Skills from './components/pages/Skills';
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
             <Route exact path='/' element={<Home />} />
             <Route path='about' element={<About />} />
             <Route path='projects' element={<Projects />} />
-            <Route path='contact' element={<Contact />} />
+            <Route path='contact' element={<Skills />} />
           </Routes>
         </div>
         <Footer />
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,12 @@ function Header() {
     return (
         <header class='header'>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand class='link1' id='nav-link' as={Link} to='/'>SW</Navbar.Brand>
+                <Navbar.Brand class='link1' as={Link} to='/'>SW</Navbar.Brand>
                 <Container class='nav'>
                     <Nav className="justify-content-end">
                         <Link class='link2' id='nav-link' as={Link} to='/about'>About</Link>
                         <Link class='link3' id='nav-link' as={Link} to='/projects'>Projects</Link>
-                        <Link class='link4' id='nav-link' as={Link} to='/contact'>Contact Me</Link>
+                        <Link class='link4' id='nav-link' as={Link} to='/skills'>Skills</Link>
                         <a id='nav-link' href='https://www.dropbox.com/s/mi2z27rvgiu77cd/Sam%20Wallace%20Resume.pdf?dl=0' target='_blank' rel='noopener noreferrer'>Resume</a>
                     </Nav>
                 </Container>
