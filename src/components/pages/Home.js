187712867b81f0cbc2556378b5deae4ac8961d9f
Sam W <sam@sammymayhem.com>
2023-02-13 12:25:17 -0800

    Updating titles to be animated, initial animation with bounce is working, but I want to refine it

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,17 @@
+import { motion } from 'framer-motion';
 import '../../styles/Home.css';
 
 function Home() {
     return (
         <div>
             <div id='home-text'>
-                <h1 id='home-text-1'>Sam Wallace.</h1>
-                <h2 id='home-text-2'> A Front End Web Developer kinda guy.</h2>
+                <motion.div
+                    initial={{ opacity: 0, x: '-100vh'}}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ type: 'spring', duaration: 2 }}>
+                    <h1 id='home-text-1'>Sam Wallace.</h1>
+                    <h2 id='home-text-2'> A Front End Web Developer kinda guy.</h2>
+                </motion.div>
             </div>
         </div>
     );
