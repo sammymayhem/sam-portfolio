9da92c5e706ae3cf0911d1d9712d1968492dc481
Sam W <sam@sammymayhem.com>
2023-02-15 19:31:28 -0800

    updated home page animations and  added colored bars that slide in

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,7 @@ import '../../styles/Home.css';
 
 function Home() {
     return (
-        <div>
+        <div className='home-cont'>
             <div id='home-text'>
                 <motion.div
                     initial={{ opacity: 0, x: '-100vh' }}
@@ -19,18 +19,28 @@ function Home() {
                     <h2 id='home-text-2'> A Front End Web Developer kinda guy.</h2>
                 </motion.div>
             </div>
-            {/* <div className='home-image'>
+            <div className='home-accents'>
                 <motion.div
-                    initial={{ opacity: 0, x: '-100vh' }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ type: 'spring', duaration: 2 }}>
-                    <div className='pic-bg'>
-                        <div className='home-pic'>
-                            <img src={sam1} alt="" id="my-image" />
-                        </div>
-                    </div>
+                        initial={{ opacity: 0, x: '-100vh' }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ delay: 1, type: 'spring', duaration: 2 }}>
+                            <div className='red-bar box'></div>
                 </motion.div>
-            </div> */}
+                <motion.div
+                        initial={{ opacity: 0, x: '-100vh' }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ delay: 1.5, type: 'spring', duaration: 2 }}>
+                            <div className='orange-bar box'></div>
+                </motion.div>
+                <motion.div
+                        initial={{ opacity: 0, x: '-100vh' }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ delay: 2, type: 'spring', duaration: 2 }}>
+                            <div className='yellow-bar box'></div>
+                </motion.div>
+
+            </div>
+            
         </div>
     );
 }
