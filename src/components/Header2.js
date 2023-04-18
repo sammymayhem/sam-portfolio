ed9a7e50cfb537cc8b7d24ed8a3f1a6f3d820959
Sam W <sam@sammymayhem.com>
2023-04-18 16:26:29 -0700

    Updated resume link

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -14,7 +14,7 @@ function Header() {
                             <Link className='link2' id='nav-link' as={Link} to='/about'>About</Link>
                             <Link className='link3' id='nav-link' as={Link} to='/projects'>Projects</Link>
                             <Link className='link4' id='nav-link' as={Link} to='/skills'>Skills</Link>
-                            <a id='nav-link' href='https://www.dropbox.com/scl/fi/v05ii2htcjf3zc7wnruu9/Sam-Wallace-Resume.docx?dl=0&rlkey=ka9m9qpm7t5bzcmxil2lhtqbt' target='_blank' rel='noopener noreferrer'>Resume</a>
+                            <a id='nav-link' href='https://www.dropbox.com/s/f7x00pqleeuwz51/Sam%20Wallace%20Resume.pdf?dl=0' target='_blank' rel='noopener noreferrer'>Resume</a>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
